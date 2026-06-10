b4c86a06a8861190711ded36bd0869e74d77c1bb
pragati-s-2009 <pragati.16009@gmail.com>
2026-06-10 10:29:53 +0530

    my first cpp upload

---
+++ primec++.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+	
+	for(int i=1;i<5;i++){
+		int n=1;
+		for(int j=1;j<5;j++){
+		
+			cout<<n<<" ";
+			n=n+1;
+		}
+		
+		cout<<endl;
+	}
+	return 0;
+	}
+
+
+
