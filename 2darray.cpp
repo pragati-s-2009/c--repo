0c558e2b4ce63697461a76a8d68797cf533c22ad
pragati-s-2009 <pragati.16009@gmail.com>
2026-06-14 19:48:27 +0530

    Add files via upload

---
+++ 2darray.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+   
+    int arr[4][4]={
+           {1,2,3,4},
+           {5,6,7,8},
+           {5,4,3,7},
+           {9,8,6,5},
+           };
+    for(int i=0;i<4;i++){
+        for(int j=0;j<4;j++){
+            cout<<arr[i][j]<<"  ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
