40a4bbf353e9d3347b536e7636f2e705f70eb5f3
pragati-s-2009 <pragati.16009@gmail.com>
2026-06-25 19:49:59 +0530

    Refactor bubble sort implementation
    Removed unnecessary comments and improved formatting.

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -3,12 +3,12 @@ using namespace std;
 void bubble(int arr[],int n){
     for(int i=1;i<n;i++){
         //rounds
+        
         for(int j=0;j<n-1;j++){
             if(arr[j]>arr[j+1]){
                 swap(arr[j],arr[j+1]);
             }
         }
-        
     }
 }
 int main(){
@@ -18,7 +18,7 @@ int main(){
         cout<<"sorted array: ";
          for(int i=0;i<n;i++){
             cout<<arr[i];
-            cout<<" ";
-         };
+            cout<<" ";  };
+    
        return 0;
     }
