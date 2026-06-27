d266b92ac7ae47a05ec9b5167feef282ac66e7e7
pragati-s-2009 <pragati.16009@gmail.com>
2026-06-27 21:40:18 +0530

    Fix formatting issues in bubble_sort.cpp

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -3,7 +3,6 @@ using namespace std;
 void bubble(int arr[],int n){
     for(int i=1;i<n;i++){
         //rounds
-        
         for(int j=0;j<n-1;j++){
             if(arr[j]>arr[j+1]){
                 swap(arr[j],arr[j+1]);
